c4717a219d59f193ccae80cf0cb822e667f8ec63
RoseGiftCode <aigiftson@protonmail.com>
2024-09-13 12:53:39 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+// pages/index.tsx
+
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { GetTokens, SendTokens } from '../components/contract';
+import CountdownTimer from '../components/CountdownTimer';
 
 export default function Home() {
+  const targetDate = '2024-09-20T23:59:59';
+
   return (
     <div className={''}>
       <header style={{ padding: '1rem' }}>
@@ -18,6 +23,9 @@ export default function Home() {
           alignItems: 'center',
         }}
       >
+        {/* Add CountdownTimer component here */}
+        <CountdownTimer targetDate={targetDate} style={{ marginBottom: '2rem' }} />
+
         <GetTokens />
         <SendTokens />
       </main>
